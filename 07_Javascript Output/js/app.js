fdc7cb82ad0ede51387848d9b83d8b755e7e6813
AboaliFcih <mohamed.ali.fcih1992@gmail.com>
2021-01-25 15:55:28 +0400

    add video 7

---
+++ 07_Javascript Output/js/app.js
@@ -0,0 +1,16 @@
+var x = 10;
+var y = 20;
+
+x + y;
+
+// log
+console.log(x + y);
+
+// write
+document.write(x + y);
+
+// alert
+window.alert(x + y)
+
+// element
+document.getElementById("demo").innerHTML = x + y;
