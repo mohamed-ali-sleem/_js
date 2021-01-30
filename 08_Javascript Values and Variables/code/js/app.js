2acc18ea272a27959de26c26385a6d469cd8c4af
AboaliFcih <mohamed.ali.fcih1992@gmail.com>
2021-01-30 15:06:30 +0400

    add 08

---
+++ 08_Javascript Values and Variables/code/js/app.js
@@ -0,0 +1,50 @@
+10;
+"mohamed";
+
+// how to define var
+var firstName = "ahmed";
+// how to use it
+console.log(firstName);
+
+
+// dont use
+// var 2name = "ahmed";
+// var function = "getVal";
+// var first&last = "mohamed ali"
+
+
+// use
+var $firstName = "mohamed";
+var _firstName = "mohamed";
+
+var firstName = "ahmed";
+var lastNameValue = "mohamed";
+var last_name_value = "islam";
+
+const PI = 3.14;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
