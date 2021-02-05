911ca860ec313c981372cabfcce19f88fe1ce69d
AboaliFcih <mohamed.ali.fcih1992@gmail.com>
2021-02-06 01:18:01 +0400

    add let var const

---
+++ 10_Javascript Let vs var vs const/code/js/app.js
@@ -0,0 +1,11 @@
+var firstName = "mohamed";
+firstName = "ahmed"; // re-assign
+
+let lastName = "ali";
+lastName = "sleem"; // re-assign
+
+const birthDate = 1990; 
+// birthDate = 1992; // not allowed 
+
+let age = 30;
+age = 31;
